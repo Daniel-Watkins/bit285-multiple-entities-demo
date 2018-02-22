a31dcfae21fc38656eb3e2cba094a763c0693a98
DanielWatkins <student@180-CLIENTVM>
2018-02-22 15:17:35 -0800

    Not finished

---
+++ bit285-multiple-entities-demo/ViewModels/AddBookViewModel.cs
@@ -0,0 +1,23 @@
+﻿using IndyBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace IndyBooks.ViewModels
+{
+    public class AddBookViewModel
+    {
+        public int AuthorID { get; set; }
+        public string Title { get; set; }
+        public string Edition { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        [Display(Name = "Publication Year")]
+        public string Publication { get; set; }
+
+        public IEnumerable<Author> Authors { get; set; }
+    }
+}
