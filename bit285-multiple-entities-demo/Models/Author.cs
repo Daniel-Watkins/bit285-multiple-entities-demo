0f81fbeb18ea3d5b1242329b5b86b3a7ec855921
Watkins, Daniel R <danielwatkins@student.cascadia.edu>
2018-03-08 09:41:36 -0800

    temp

--- bit285-multiple-entities-demo/Models/Author.cs
+++ bit285-multiple-entities-demo/Models/Author.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace IndyBooks.Models
 {
     public class Author
     {
+        [Key]
         public int AuthorID { get; set; }
 
         public string FirstName { get; set; }
